41d1c85a2375450ebcfc4d8eb3f5e57dde587658
Barbora Pomykalová <xpomyk03@vutbr.cz>
2021-11-11 13:06:33 +0100

    Pridani funkce na indikace vzestupnych a sestupnych casti.

--- cviceni_7.R
+++ cviceni_7.R
@@ -1,13 +1,35 @@
-indexstart <- function(permutacni_vecktor){
+NajdiSetridene <- function(permutacni_vektor){
   k = 0;
-  for (i in 2 : length(permutacni_vecktor)){
+  for (i in 2 : length(permutacni_vektor)){
     index = i;
     k = k + i-1;
-    if (sum(permutacni_vecktor[1:i]) == k){
+    if (sum(permutacni_vektor[1:i]) == k){
       next
     }else{
       break
     }
   }
   return(index)
+}
+
+
+Vzestupne <- function(permutacni_vektor){
+  
+  vektor_indikaci <- numeric(length(permutacni_vektor))
+  vektor_indikaci[c(1,length(permutacni_vektor))] <- 1
+  
+  for (i in 2:length(permutacni_vektor)){
+    if (permutacni_vektor[i] == (permutacni_vektor[i-1]+1)){
+      vektor_indikaci[c(i-1,i)] <- 1
+    }else{
+      next
+    }
+  }
+  return(vektor_indikaci)
+}
+
+
+
+BreakPointSort <- function(){
+  
 }
